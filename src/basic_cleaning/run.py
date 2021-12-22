3b32145b40b551e93d2a25dece547ee41355a5b6
J. R. Verbiest <39417102+JrVerbiest@users.noreply.github.com>
2021-12-22 17:00:56 +0100

    EDA & Data cleaning
    Project. Perform step 1 (EDA) and step 2 (data cleaning)

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 """
- Performs basic cleaning on the data and save the results  in Weights & Biases
+ Download from W&B the raw dataset and apply some basic data cleaning,
+ export the result to a new artifact
 """
 import argparse
 import logging
+
+from numpy.core.fromnumeric import argsort
 import wandb
+import os
+import pandas as pd
 
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
@@ -18,36 +23,84 @@ def go(args):
 
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    logger.info("download artifact: %s", args.input_artifact)
+    df = pd.read_csv(artifact_local_path)
+
+    logger.info("remove outliers (min-max): %i - %i", args.min_price, args.max_price)
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df["price"].between(min_price, max_price)
+    df = df[idx].copy()
+
+    tmp_dir = os.path.join(args.tmp_dir,args.output_artifact)
+    logger.info("save clean artifact, path: %s", tmp_dir)
+    df.to_csv(tmp_dir, index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
+    )
+
+    artifact.add_file(tmp_dir)
+    run.log_artifact(artifact)
+
+    artifact.wait()
+    logger.info("artifact uploaded")
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="This steps cleans the data")
 
-
     parser.add_argument(
-        "--parameter1", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--tmp_dir",
+        type=str,
+        help="temporary directory",
         required=True
     )
 
     parser.add_argument(
-        "--parameter2", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--input_artifact",
+        type=str,
+        help="input artifact name (*.csv)",
         required=True
     )
 
     parser.add_argument(
-        "--parameter3", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        "--output_artifact",
+        type=str,
+        help="output artifact name (*.csv)",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type",
+        type=str,
+        help="output artifact type",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description",
+        type=str,
+        help="output artifact description",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price",
+        type=int,
+        help="the minimum price - outlier left (min.) boundary",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price",
+        type=int,
+        help="the maximum price - outlier right (max.) boundary",
         required=True
     )
 
